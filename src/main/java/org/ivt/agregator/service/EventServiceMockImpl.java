4b9334abcd0df7311fd54f3b7bdd6978ed6c0aca
Andrey Ku <andreykudr@yahoo.com>
2016-11-02 23:36:51 +0300

    CI test

--- src/main/java/org/ivt/agregator/service/EventServiceMockImpl.java
+++ src/main/java/org/ivt/agregator/service/EventServiceMockImpl.java
@@ -26,7 +26,7 @@ public class EventServiceMockImpl implements EventService {
 
     private Event getEvent(String name, Address address, GregorianCalendar endTime, GregorianCalendar beginTime) {
         Event event = new Event();
-        event.setId(1);
+        event.setId(1l);
         event.setName(name);
         event.setAddress(address);
         event.setBeginTime(beginTime);
