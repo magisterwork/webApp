ca9d674933b7e145242ebe6714ca290a828cca4e
Andrey Ku <andreykudr@yahoo.com>
2016-11-07 23:50:46 +0300

    Подключен Jackson. Не показывать null значения в json.

--- src/main/java/org/ivt/agregator/entity/Event.java
+++ src/main/java/org/ivt/agregator/entity/Event.java
@@ -1,8 +1,13 @@
 package org.ivt.agregator.entity;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.Calendar;
 import java.lang.Long;
 
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
+
+@JsonInclude(Include.NON_NULL)
 public class Event {
 
     private Long id;
